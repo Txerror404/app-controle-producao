34ceacb22a98671efd05c17c9f18dc8e86e646b2
Txerror404 <williamclaiton@msn.com>
2026-03-06 19:54:52 -0300

    Update utils.py

--- fabrica_app/utils.py
+++ fabrica_app/utils.py
@@ -9,10 +9,12 @@ def verificar_conflito(df, maquina, inicio_novo, fim_novo):
     if df.empty:
         return False
 
-    df_maquina = df[df["maquina"] == maquina]
+    df_maquina = df[
+        (df["maquina"] == maquina) &
+        (df["status"].isin(["Pendente", "Setup", "Manutenção"]))
+    ]
 
     for _, row in df_maquina.iterrows():
-
         inicio_existente = row["inicio"]
         fim_existente = row["fim"]
 

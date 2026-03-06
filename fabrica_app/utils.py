49ad775ba7810ec7a352850d4202e6056e48ff90
Txerror404 <williamclaiton@msn.com>
2026-03-06 20:21:48 -0300

    Update utils.py

--- fabrica_app/utils.py
+++ fabrica_app/utils.py
@@ -1,24 +1,20 @@
-from datetime import datetime
-
 def verificar_conflito(df, maquina, inicio_novo, fim_novo):
-    """
-    Verifica se existe conflito de horário na mesma máquina.
-    Retorna True se houver conflito.
-    """
 
     if df.empty:
         return False
 
     df_maquina = df[
-        (df["maquina"] == maquina) &
-        (df["status"].isin(["Pendente", "Setup", "Manutenção"]))
+        (df["maquina"] == maquina)
+        & (df["status"].isin(["Pendente","Setup","Manutenção"]))
     ]
 
     for _, row in df_maquina.iterrows():
+
         inicio_existente = row["inicio"]
         fim_existente = row["fim"]
 
         if (inicio_novo < fim_existente) and (fim_novo > inicio_existente):
+
             return True
 
     return False

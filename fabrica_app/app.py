7dfc397e8266a3a35b81be1579279631d6e1ed90
Txerror404 <williamclaiton@msn.com>
2026-03-07 15:39:00 -0300

    Update app.py

--- fabrica_app/app.py
+++ fabrica_app/app.py
@@ -39,7 +39,7 @@ st.sidebar.write("Banco em uso:", DB_PATH)
 # FUNÇÃO DE CONEXÃO
 # =================================================================
 
-DATABASE_URL = "postgresql://postgres.ogxrgnaedmcbaqgryosg:[pcp2026supabase]@aws-0-us-west-2.pooler.supabase.com:5432/postgres"
+DATABASE_URL = "postgresql://postgres.ogxrgnaedmcbaqgryosg:[pcp2026supabase]@aws-0-us-west-2.pooler.supabase.com:6543/postgres"
 
 def conectar():
     return psycopg2.connect(DATABASE_URL)

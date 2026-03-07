b3a903cb9b08bf887739007e77ace142f334cbc6
Txerror404 <williamclaiton@msn.com>
2026-03-07 18:57:57 -0300

    Update serigrafia.py

--- fabrica_app/pages/serigrafia.py
+++ fabrica_app/pages/serigrafia.py
@@ -1,1 +1,10 @@
+# =================================================================
+# ABA 1 - CRONOGRAMA SERIGRAFIA
+# =================================================================
 
+import streamlit as st
+from config import MAQUINAS_SERIGRAFIA
+from components import renderizar_setor
+
+def mostrar_serigrafia():
+    renderizar_setor(MAQUINAS_SERIGRAFIA)
